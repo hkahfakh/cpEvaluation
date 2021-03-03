a9d5215776935232be7eee842cdc5096896c076a
hkahfakh <you@example.com>
2021-03-03 22:14:55 +0800

    删掉了mrmr中冗余的代码

--- featuresAlgorithm/mRMR.py
+++ featuresAlgorithm/mRMR.py
@@ -1,5 +1,4 @@
 # coding:utf-8
-
 from sklearn import metrics
 
 
@@ -7,15 +6,12 @@ class mRMR:
     """
     具有m个特征的特征子集S
     这些只需要处理训练集  测试集当然需要处理了   你筛选出特征来测试集那么多特征怎么用   F是训练集  原始特征
-
     """
 
-    def __init__(self, F, C, test_X, test_y, m=3):
+    def __init__(self, F, C, m=3):
         self.F = F
         self.C = C  # 标签
-        self.test_X = test_X
-        self.test_y = test_y
-        self.S = list()  # 已选特征的下标
+        self.selectedFeature = list()  # 已选特征的下标
         self.m = m  # 要选取m个特征  最小为1
 
     def cal_mi_sum(self, fi):
@@ -26,14 +22,14 @@ class mRMR:
         :return: 返回这些互信息的和
         """
         sum = 0.0
-        for i in range(len(self.S)):
-            fj = self.F[:, self.S[i]]
+        for i in range(len(self.selectedFeature)):
+            fj = self.F[:, self.selectedFeature[i]]
             MI = metrics.mutual_info_score(fi, fj)
             sum = sum + MI
         return sum
 
     def mrmr(self):
-
+        S = list()
         row, col = self.F.shape
 
         F_MI = list()
@@ -49,7 +45,7 @@ class mRMR:
         # 互信息最大的作为第一个特征
         maxindex = max(F_MI)
         i = F_MI.index(maxindex)
-        self.S.append(F_MI_index[i])  # S里存的是原集合的下标
+        S.append(F_MI_index[i])  # S里存的是原集合的下标
         F_MI.pop(i)
         F_MI_index.pop(i)
         self.m = self.m - 1  # 表示已经找到一个特征了
@@ -62,20 +58,18 @@ class mRMR:
                 # 但是如果删掉  就无法对应他是第几个了   所以需要两个列表 index，他与标签的互信息
                 fi = self.F[:, F_MI_index[t1]]
                 sum = self.cal_mi_sum(fi)
-                difference = F_MI[t1] - sum/len(self.S)
+                difference = F_MI[t1] - sum / len(S)
                 MI_fi_S.append(difference)
 
             maxindex = MI_fi_S.index(max(MI_fi_S))  # 找到这些和里面最大的那个
-            self.S.append(F_MI_index[maxindex])  # 把特征的原始索引加到已选特征里
+            S.append(F_MI_index[maxindex])  # 把特征的原始索引加到已选特征里
             F_MI.pop(maxindex)
             F_MI_index.pop(maxindex)
-        print(self.S)
-
-        return self.S
+        return S
 
     def mrmr2classifer(self):
-        return self.F[:, self.S], self.C, self.test_X[:, self.S], self.test_y
+        return self.F[:, self.selectedFeature], self.C
 
     def mrmr_thread(self):
-        self.mrmr()
+        self.selectedFeature = self.mrmr()
         return self.mrmr2classifer()
